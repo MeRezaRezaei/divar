7a3e5d42b58a978a3cf939759cb4a7e99c6e0995
Reza Rezaei <merezarezaei@gmail.com>
2021-07-14 16:08:45 +0430

    cateegories parent id was not set as nullable bug fixed

--- database/migrations/2021_07_13_142844_create_categories_table.php
+++ database/migrations/2021_07_13_142844_create_categories_table.php
@@ -15,7 +15,7 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->unsignedBigInteger('parent_id');
+            $table->unsignedBigInteger('parent_id')->nullable();
             $table->string('name');
             $table->timestamps();
             $table->foreign('parent_id')->references('id')->on('categories');
