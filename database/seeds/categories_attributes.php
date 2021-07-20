7623567dba2fc9891b6c7aa6f023d6d1138ff4f5
Reza Rezaei <merezarezaei@gmail.com>
2021-07-20 15:14:22 +0430

    add some seeders for testing post and admin part

--- database/seeds/categories_attributes.php
+++ database/seeds/categories_attributes.php
@@ -19,16 +19,23 @@ class categories_attributes extends Seeder
             "phone"=> "989371030532",
             "first_name"=> "Reza",
             "last_name"=> "Rezaei",
-            "password"=> "testtest"
+            "password"=> sha1("testtest")
         ]);
 
-        $place1 = Places::Create([
-            'name' => 'shiraz'
-        ]);
 
         $categories1 = Category::Create([
         'name' => 'RealEstate',
         ]);
+
+        $categories11 = Category::Create([
+        'name' => 'apartment',
+        'parent_id' => $categories1->id,
+        ]);
+
+        $categories111 = Category::Create([
+            'name' => 'kitchen',
+            'parent_id' => $categories11->id,
+            ]);
         
         $categories2 = Category::Create([
             'name' => 'Car',
@@ -40,8 +47,13 @@ class categories_attributes extends Seeder
         ]);
 
         $attributes1 = Attributes::Create([
+            'name'=>'balcony_area',
+            'category_id' => $categories11->id,
+            'is_required' => true,
+        ]);
+        $attributes1 = Attributes::Create([
             'name'=>'area',
-            'category_id' => $categories1->id,
+            'category_id' => $categories111->id,
             'is_required' => false,
         ]);
         $attributes2 = Attributes::Create([          
@@ -81,6 +93,31 @@ class categories_attributes extends Seeder
             'name'=>'year',
             'category_id' => $categories2->id,
             'is_required' => false,
+        ]);
+
+
+        $place1 = Places::Create([
+            'name'=>'fars'
+        ]);
+
+        $place2 = Places::Create([
+            'name'=>'shiraz',
+            'parent_id' => $place1->id,
+        ]);
+
+        $place3 = Places::Create([
+            'name'=>'zand',
+            'parent_id' => $place2->id,
+        ]);
+
+        $place4 = Places::Create([
+            'name'=>'marvdasht',
+            'parent_id'=> $place1->id,
+        ]);
+
+        $place4 = Places::Create([
+            'name'=>'meydan aval',
+            'parent_id'=> $place4->id,
         ]);
 
 
